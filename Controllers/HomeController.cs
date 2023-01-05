bb6580d1e5a2d9ae53591523bd73004d01d29d39
Andrew Kritzler <KritzlerA@adr.org>
2023-01-05 16:45:47 -0500

    HTML to PDF demo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SyncfusionTicketDemo.Models;
+using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using System.IO;
 
 namespace SyncfusionTicketDemo.Controllers;
 
@@ -27,5 +30,26 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public ActionResult ExportToPDF()
+    {
+        //Initialize HTML to PDF converter.
+        HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+        BlinkConverterSettings settings = new BlinkConverterSettings();
+        //Set command line arguments to run without the sandbox.
+        settings.CommandLineArguments.Add("--no-sandbox");
+        settings.CommandLineArguments.Add("--disable-setuid-sandbox");
+        //Set Blink viewport size.
+        settings.ViewPortSize = new Syncfusion.Drawing.Size(1280, 0);
+        //Assign Blink settings to the HTML converter.
+        htmlConverter.ConverterSettings = settings;
+        //Convert URL to PDF document.
+        PdfDocument document = htmlConverter.Convert("https://www.syncfusion.com");
+        //Create memory stream.
+        MemoryStream stream = new MemoryStream();
+        //Save the document to memory stream.
+        document.Save(stream);
+        return File(stream.ToArray(), System.Net.Mime.MediaTypeNames.Application.Pdf, "HTML-to-PDF.pdf");
     }
 }
